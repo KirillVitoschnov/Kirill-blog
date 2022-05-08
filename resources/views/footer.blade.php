fad7ea64990d37087b243acf9b64c3118fd081de
Kirill <kirillkekovalmaty@gmail.com>
2022-05-08 20:59:23 +0600

    initial comit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="footer-main">
+
+</div>
+</body>
+</html>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,14 @@
+<div class="header-main">
+    <nav class="header-main-links">
+        <a href="">Программы</a>
+        <a href="">Категории</a>
+        <a href="">Новости</a>
+        <a href="">Бла Бла</a>
+
+    </nav>
+    <div class="header-main-user">
+        <a>Войти</a>
+        <a>Зарегистрироваться</a>
+    </div>
+</div>
+
